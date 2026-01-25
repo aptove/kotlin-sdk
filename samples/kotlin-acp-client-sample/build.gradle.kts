fb3cdf1264b04eb6891c7961961680b7345279bd
saltukalakus <saltukalakus@gmail.com>
2026-01-25 20:35:59 +0000

    Fix WebSocketClientApp dependencies: add ktor-client and WebSocket support

--- samples/kotlin-acp-client-sample/build.gradle.kts
+++ samples/kotlin-acp-client-sample/build.gradle.kts
@@ -6,9 +6,14 @@ plugins {
 
 dependencies {
     implementation(project(":acp"))
+    implementation(project(":acp-ktor"))
+    implementation(project(":acp-ktor-client"))
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlin.logging)
     implementation(libs.kotlinx.io.core)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.websockets)
+    implementation("io.ktor:ktor-client-cio:3.1.3")
     implementation("ch.qos.logback:logback-classic:1.5.13")
 }
 
